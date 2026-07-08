20844fb5988c78d269bfed60ae9610cc0ab9dd4e
Thiago Nascimento Barbosa <thiagonascimento.barbosapro@gmail.com>
2026-07-08 19:03:47 -0300

    fix: remove .tmp e preventivas 2025 do tracking

--- 5 - Arquivo morto/05 - Preventivas 2025/Automações/- Protótipo Aplicação de levantamento (tecnologia de nivel de pasta)/executar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Servidor de Inteligência MAFFENG
-cd /d %~dp0
-echo ======================================
-echo Iniciando Servidor Flask (tempo real)
-echo ======================================
-python main_tk.py
-pause
